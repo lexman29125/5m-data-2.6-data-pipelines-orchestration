7ac449917bb4a68222cb3fef87ae59eb41dfa0d8
MLNotes2718 <150231238+mlnotes2718@users.noreply.github.com>
2025-08-16 16:14:24 +0800

    Update prices.sql
    update solution for prices.sql

--- solutions/resale_flat/models/prices.sql
+++ solutions/resale_flat/models/prices.sql
@@ -2,18 +2,18 @@
 
 WITH RAW AS (
     SELECT
-        month_of_sale,
+        month,
         town,
         flat_type,
         block,
         street_name,
         storey_range,
-        parse_numeric(floor_area_sqm) AS floor_area_sqm,
+        CAST(floor_area_sqm AS FLOAT64) AS floor_area_sqm,
         flat_model,
         lease_commence_date,
         remaining_lease,
-        resale_price
-    FROM {{ source('ingestion', 'public_resale_flat_prices_from_jan_2017') }}
+        CAST(resale_price AS FLOAT64) AS resale_price
+    FROM {{ source('resale', 'public_resale_flat_prices_from_jan_2017') }}
 )
 SELECT
     *,
